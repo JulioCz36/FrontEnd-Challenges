46d0718edf68d9ed0268a08601dd84697d87354d
JulioCz36 <rodriguez.julio.cesar.322@gmail.com>
2024-05-13 02:28:38 -0300

    actualizacion

--- 5-Random quote generator/src/app/app.component.ts
+++ 5-Random quote generator/src/app/app.component.ts
@@ -1,17 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
   title = 'Random-quote-generator';
-  
-  constructor(private router: Router) {}
 
-  ngOnInit(): void {
-    this.router.navigate(['/home']); // Redirige al menú al inicializar
-  }
 }
